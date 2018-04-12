c6905da44fed5b16e177fd86657123668a9f385a
Bastien Mazeran <bastien.mazeran@autodesk.com>
2018-04-12 09:54:37 -0700

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   STORAGE_BASE_ENDPOINT: 'https://developer.api.autodesk.com/data/v1',
   SCOPES: ['bucket:create','bucket:read','data:create','data:read','data:write'],
   // Redis settings
-  REDIS_ENDPOINT: 'redis-19812.c15.us-east-1-2.ec2.cloud.redislabs.com',
+  REDIS_ENDPOINT: '<your redis endpoint>.cloud.redislabs.com',
   REDIS_PORT: 19812,
   // Autodesk Data Management settings
   BUCKET_KEY: 'reality-capture-output',
