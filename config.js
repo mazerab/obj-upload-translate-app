e8e247fa207a9baf221ef58c2e7b6cd0b68b5612
Bastien Mazeran <bastien.mazeran@autodesk.com>
2018-04-23 13:46:43 -0700

    Update config.js

--- config.js
+++ config.js
@@ -7,19 +7,15 @@ module.exports = {
   // Redis settings
   REDIS_ENDPOINT: '<your redis endpoint>.cloud.redislabs.com',
   REDIS_PORT: 19812,
-  
   // DO NOT EDIT BELOW THIS LINE
-  
   // Autodesk Forge settings
   DERIVATIVE_BASE_ENDPOINT: 'https://developer.api.autodesk.com/modelderivative/v2',
   OSS_BASE_ENDPOINT: 'https://developer.api.autodesk.com/oss/v2',
   STORAGE_BASE_ENDPOINT: 'https://developer.api.autodesk.com/data/v1',
   SCOPES: ['bucket:create','bucket:read','data:create','data:read','data:write'],
-
   // Autodesk Data Management settings
   BUCKET_KEY: 'reality-capture-output',
   RECAP_OUTPUT_FILE_PATH: '/tmp/reality-capture-output.zip',
   BUBBLES_OUTPUT_DIR: '/tmp/derivatives',
   BUBBLES_FILE_PATH: '/tmp/derivatives.zip',
-
 };
